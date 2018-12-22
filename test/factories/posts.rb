7265fd759957bd230fbef35fe0b077e1cd3fe769
Mario Vizcaino <mario.vizcainos@gmail.com>
2018-12-21 22:05:32 -0500

    creation of factory bot

---
+++ test/factories/posts.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :post do
+    title { Faker::Lorem.sentence }
+    content { Faker::Lorem.paragraph }
+    published { 
+      r = rand(0..1)
+      r == 0
+     }
+    user
+  end
+end
