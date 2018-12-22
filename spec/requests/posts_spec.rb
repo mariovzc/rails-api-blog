95d9ba9f727bf738f2a712ddae85a1d255e35276
Mario Vizcaino <mario.vizcainos@gmail.com>
2018-12-21 22:22:32 -0500

    fixed posts spec

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -15,17 +15,16 @@ RSpec.describe 'Posts', type: :request do
     it 'should return all the published posts' do
       get '/posts'
       payload = JSON.parse(response.body)
-      expect(payload.size).to eq(post.size)
+      expect(payload.size).to eq(posts.size)
       expect(response).to have_http_status(200)
     end
   end
 
   describe 'GET /posts/{id}' do
-    let(:post) { create(:post) }
+    let!(:post) { create(:post) }
 
     it 'should return a post' do
-      get "/posts/#{post.id}" 
-
+      get "/posts/#{post.id}"
       payload = JSON.parse(response.body)
       expect(payload).to_not be_empty
       expect(payload['id']).to eq(post.id)
