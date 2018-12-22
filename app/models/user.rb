b221b255661300915d6324af74ccbd24d9b855ee
Mario Vizcaino <mario.vizcainos@gmail.com>
2018-12-21 21:32:17 -0500

    added models validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,19 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  email      :string
+#  name       :string
+#  auth_token :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class User < ApplicationRecord
+  validates :email, presence: true
+  validates :name, presence: true
+  validates :auth_token, presence: true
+
+  has_many :posts
 end
