8e25d60591391615755ab8979708c2c952b8d01c
Mario Vizcaino <mario.vizcainos@gmail.com>
2018-12-26 09:01:40 -0500

    added create and update posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,17 @@
 class PostsController < ApplicationController
+
+  # Error handler, bottom up priority
+
+  # catch all app exceptions
+  rescue_from Exception do |e|
+    render json: {error: e.message}, status: :internal_error
+  end
+
+  # only active record validation execption
+  rescue_from ActiveRecord::RecordInvalid do |e|
+    render json: {error: e.message}, status: :unprocessable_entity
+  end
+
   # GET /posts
   def index
     @posts = Post.where(published: true)
@@ -11,5 +24,27 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     render json: @post,
            status: :ok
+  end
+
+  # POST /posts
+  def create
+    @post = Post.create!(create_params)
+    render json: @post, status: :created
+  end
+
+  # PUT /posts/{id}
+  def update
+    @post = Post.find(params[:id])    
+    @post.update!(update_params)
+    render json: @post, status: :ok
+  end
+
+  private
+  def create_params
+    params.require(:post).permit(:title, :content, :published, :user_id)
+  end
+
+  def update_params
+    params.require(:post).permit(:title, :content, :published)
   end
 end
