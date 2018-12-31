a7979137b74bd68997371ba6953c4ac04e9101ef
Mario Vizcaino <mario.vizcainos@gmail.com>
2018-12-31 09:27:39 -0500

    generation of auth token for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,9 +11,20 @@
 #
 
 class User < ApplicationRecord
+  has_many :posts
+
   validates :email, presence: true
   validates :name, presence: true
   validates :auth_token, presence: true
 
-  has_many :posts
+  after_initialize :generate_auth_token
+
+  def generate_auth_token
+    # User.new
+    # if !auth_token.present?
+    unless auth_token.present?
+      # generate token
+      self.auth_token = TokenGenerationService.generate
+    end
+  end
 end
